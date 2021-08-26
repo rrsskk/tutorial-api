3f11f8df54b04cfa416215f03c64f5202a88b328
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-26 18:29:29 +0530

    Added needy,Optional & default parameter at the same time

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-
+from typing import Optional
 from fastapi import FastAPI
-
 
 
 app = FastAPI()
 
 
 @app.get("/item/{item_id}")
-async def read_user_item(item_id :str,needy:str):
-    item = {"item_id":item_id,"needy":needy}
+async def read_user_item(item_id :str,needy:str,skip:int=0,limit:Optional[int]=None):
+    item = {"item_id":item_id,"needy":needy,"skip":skip,"limit":limit}
     return item
 
 
