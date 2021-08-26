adae6c11d10c036b2d416f8c519adb00229acb22
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-26 17:14:30 +0530

    Declared multiple parameter and query parameter at same time

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/item/{item_id}")
-async def read_item(item_id :str,q : Optional[str]=None):
+@app.get("/user/{user_id}/item/{item_id}")
+async def read_item(user_id:int,item_id :str,q : Optional[str]=None ,short:bool =False):
+    item = {"owner_id":user_id,"item_id":item_id}
     if q:
-        return {"item_id":item_id,"q":q}
-    return {"item_id": item_id}
+        item.update({"q":q})
+    if not short:
+        item.update({"description":"This is am amazing item that have long descriprtion"})
+    return item
 
 
    
