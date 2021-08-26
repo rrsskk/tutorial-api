cf52f6b7fcf8812447195f1ac44ef08ceaf6c4a2
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-27 00:10:40 +0530

    Declared request body and path parameter at same time

--- main.py
+++ main.py
@@ -5,20 +5,15 @@ from fastapi import FastAPI
 class Item (BaseModel):
     name : str
     description: Optional[str] = None
-    price : int
+    price : float
     tax : Optional[float] = None
 
 app = FastAPI()
 
 
-@app.post("/item/")
-async def create_item(item:Item):
-  item_dict = item.dict()
-  if Item.tax:
-    price_with_tax = Item.tax + Item.price
-    item_dict.update({"price_with_tax":price_with_tax})
-      
-    return item_dict 
+@app.post("/item/{item_id}")
+async def create_item(item_id:int,item:Item):
+    return {"item_id":item_id,**item.dict()} 
      
 
 
