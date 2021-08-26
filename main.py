678bc1597fedafa361ba07d44bf441457fd0ea81
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-26 18:01:42 +0530

    Added needy as query parameter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-from typing import Optional
 from fastapi import FastAPI
 
 
@@ -7,13 +6,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/user/{user_id}/item/{item_id}")
-async def read_item(user_id:int,item_id :str,q : Optional[str]=None ,short:bool =False):
-    item = {"owner_id":user_id,"item_id":item_id}
-    if q:
-        item.update({"q":q})
-    if not short:
-        item.update({"description":"This is am amazing item that have long descriprtion"})
+@app.get("/item/{item_id}")
+async def read_user_item(item_id :str,needy:str):
+    item = {"item_id":item_id,"needy":needy}
     return item
 
 
