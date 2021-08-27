8d8e57078368dc25edd9f6f3174b2da25872d282
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-27 14:58:02 +0530

    Added greater than equal (ge) in path for numeric validation

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_item(*,item_id:int=Path(...,title="The id of item to get"),
+async def read_item(*,item_id:int=Path(...,title="The id of item to get",ge=1),
     q:str):
     
     result = {"item_id":item_id}
