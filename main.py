aa2444449b71743b400cbdcfb79f6b4816978a20
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-27 12:06:41 +0530

    Passed fixedquery as default parameter without Optional

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_item(q:Optional[str]=Query(None,min_length=3,max_length=50,regex="^fixedquerry$")):
+async def read_item(q:str=Query("fixedquery",min_length=3,)):
     result= {"item_id":[{"item_id":"Foo"},{"item_id":"Bar"}]} 
     if q:
         result.update({"q":q})
