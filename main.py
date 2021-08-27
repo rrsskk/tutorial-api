dc137afaa5194146a415e72e5326064343b9325b
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-27 10:32:03 +0530

    Declared query parameter & string validation

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-from pydantic import BaseModel
+
 from typing import Optional
 from fastapi import FastAPI
 
-class Item (BaseModel):
-    name : str
-    description: Optional[str] = None
-    price : float
-    tax : Optional[float] = None
 
 app = FastAPI()
 
 
-@app.post("/item/{item_id}")
-async def create_item(item_id:int,item:Item,q:Optional[str]=None):
-    result= {"item_id":item_id,**item.dict()} 
+@app.get("/items/")
+async def create_item(q:Optional[str]=None):
+    result= {"item_id":[{"item_id":"Foo"},{"item_id":"Bar"}]} 
     if q:
-        result.update({q:q})
+        result.update({"q":q})
 
         return result
 
