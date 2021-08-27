ceba8e6de81bff711108b26e5ea12144ba421c9a
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-27 18:19:01 +0530

    Imported list as tags

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 
-from typing import Optional
+from typing import Optional,List
 from fastapi import FastAPI,Body
-from pydantic import BaseModel,Field
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 class Item (BaseModel):
     name:str
-    description:Optional[str]=Field(None,title="The description of item",max_length=1000)
+    description:Optional[str]=None
     price:float
-    tax:Optional[float]=Field(...,gt=0,description="The price must be greater than zero")
+    tax:Optional[float] = None
+    tags:List[str]=[]
 
 
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id:int,item:Item= Body(...,embed=True)):
+async def update_item(item_id:int,item:Item):
     
     result = {"item_id": item_id,"item":item}
     
