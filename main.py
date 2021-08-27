a39538f27a61573b105d31c1af9e0efddafe54af
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-27 15:18:15 +0530

    Added size for numerial validation with float value

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 
 
-from fastapi import FastAPI,Path
+from fastapi import FastAPI,Path,Query
 
 
 app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_item(*,item_id:int=Path(...,title="The id of item to get",ge=1),
-    q:str):
+async def read_item(*,
+item_id: int=Path(...,title="The id of item to get",ge=0,le=100),
+    q:str, 
+    Size:float= Query(...,gt=0,lt=10.5)):
     
-    result = {"item_id":item_id}
+    result = {"item_id": item_id}
     if q:
+         
         result.update({"q": q})
     return result
 
