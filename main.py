1a412eef5e41784fcc4a6c4334fd362cc8fe1d1a
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-27 10:46:24 +0530

    Imported Query in Optional

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def create_item(q:Optional[str]=None):
+async def read_item(q:Optional[str]=Query(None,max_length=50)):
     result= {"item_id":[{"item_id":"Foo"},{"item_id":"Bar"}]} 
     if q:
         result.update({"q":q})
