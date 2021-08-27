ec090a46e23f03caf8a29b614016ae8211ebefc7
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-27 13:31:25 +0530

    Added deprecated in Query along with others

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_item(q:Optional[str]=Query(None,alias="item-query",min_length=3)):
+async def read_item(q:Optional[str]=Query(None,title="item_query",description="This is matched query parameter",
+alias="item-query",min_length=3,max_length=50,regex="^item_query$",deprecated=True)):
     
     result = {"items":[{"item_id":"Foo"},{"item_id":"Bar"}]}
     if q:
