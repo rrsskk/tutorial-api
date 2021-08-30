3b9ed2c095113909367ecf45fa4bede352aa7982
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-30 12:49:38 +0530

    Added an Output model

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ from pydantic import BaseModel, EmailStr
 app = FastAPI()
 
 
-
 class UserIn(BaseModel):
 
     username: str
 
     password: str
+    
+    email: EmailStr
+    full_name: Optional[str] = None
 
+class UserOut(BaseModel):
+    username: str
     email: EmailStr
     full_name: Optional[str] = None
 
 
-# Don't do this in production!
-@app.post("/user/", response_model=UserIn)
+@app.post("/user/", response_model=UserOut)
 async def create_user(user: UserIn):
     return user
