c6141783046c6cd50d8522516f88c9c3ee3753f3
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-30 10:28:34 +0530

    Create multiple image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from typing import Optional,Set,List
+from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel,HttpUrl
 
@@ -10,26 +10,13 @@ class Image(BaseModel):
     url:HttpUrl
     name:str
 
-class Item (BaseModel):
-    name:str
-    description:Optional[str]=None
-    price:float
-    tax:Optional[float] = None
-    tags:Set[str]= set()
-    image: Optional[List[Image]] = None
 
 
-
-class Offer (BaseModel):
-    name :str
-    description: Optional[str]=None
-    price:float
-    item:List[Item]
 
 @app.post("/offer/")
-async def create_offer(offer:Offer):
+async def create_mutiple_images(image:List[Image]):
     
-    return offer
+    return image
 
 
     
