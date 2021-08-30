6d7383dc9e7437a2f4150eaf05355d641e70a3a0
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-30 11:14:27 +0530

    Added body in example

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
-
 from typing import Optional
-from fastapi import FastAPI
-from pydantic import BaseModel,Field
 
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item (BaseModel):
-    name :str = Field(...,example="Foo")
-    description: Optional[str]= Field(None,example="Very nice")
-    price: float = Field(...,example="56.0")
-    tax:Optional[float]= Field(None,example="20.3")
 
-    
-    
-@app.put("/item/{item_id}")
-async def update_items(item_id:int,item:Item):
-    
-    result = {"item_id":item_id,"item":item}
-    return result
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 
-    
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int,
+    item: Item = Body(
+        ...,
+
+        example={
+
+            "name": "Foo",
+
+            "description": "A very nice Item",
+
+            "price": 35.4,
+
+            "tax": 3.2,
+
+        },
+
+    ),
+):
+    results = {"item_id": item_id, "item": item}
+    return results
 
 
    
