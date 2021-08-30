dfbf175f091b16dd313ee6baa0d8078df2ea34d5
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-30 17:45:03 +0530

    Imported file and form at same time using token

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
-from typing import List
+from fastapi import FastAPI, File, Form, UploadFile
 
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
 @app.post("/files/")
+async def create_file(
+    file: bytes = File(...), fileb: UploadFile = File(...), token: str = Form(...)
+):
+    return {
+        "file_size": len(file),
+        "token": token,
+        "fileb_content_type": fileb.content_type,
+    }
 
-async def create_files(files: List[bytes] = File(...)):
-
-    return {"file_sizes": [len(file) for file in files]}
-
-
-@app.post("/uploadfiles/")
-
-async def create_upload_files(files: List[UploadFile] = File(...)):
-
-    return {"filenames": [file.filename for file in files]}
-
-
-@app.get("/")
-async def main():
-    content = """
-<body>
-<form action="/files/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-</body>
-    """
-    return HTMLResponse(content=content)
 
