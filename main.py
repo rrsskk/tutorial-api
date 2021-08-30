89e9f5e3489315be70c952f602a4b1b5dc3a257a
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-30 18:22:47 +0530

    Added custom header to raise error

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ items = {"foo": "The Foo Wrestlers"}
 @app.get("/items/{item_id}")
 async def read_item(item_id: str):
     if item_id not in items:
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(status_code=404, detail="Item not found",headers={"X-Error":"There goes my error"})
     return {"item": items[item_id]}
 
 
