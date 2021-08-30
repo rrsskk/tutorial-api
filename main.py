d672b55974b7ae46130274f855d532ef6344db51
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-30 17:13:52 +0530

    Created file & Upload file

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, File, UploadFile
 
 
 app = FastAPI()
 
 
-@app.post("/login/")
-async def login(username: str = Form(...), password: str = Form(...)):
-    return {"username": username}
+@app.post("/files/")
+async def create_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
 
 
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
