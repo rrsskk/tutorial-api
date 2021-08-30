593e576e62f4f8b00cd3c75ba106108c9fe293f8
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-30 11:34:11 +0530

    Added cookie parameter

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-from datetime import datetime, time, timedelta
-
 from typing import Optional
 
-from uuid import UUID
-
-
-from fastapi import Body, FastAPI
+from fastapi import Cookie, FastAPI
 
 app = FastAPI()
 
 
-@app.put("/items/{item_id}")
-async def read_items(
+@app.get("/items/")
 
-    item_id: UUID,
+async def read_items(ads_id: Optional[str] = Cookie(None)):
 
-    start_datetime: Optional[datetime] = Body(None),
-
-    end_datetime: Optional[datetime] = Body(None),
-
-    repeat_at: Optional[time] = Body(None),
-
-    process_after: Optional[timedelta] = Body(None),
-
-):
-    start_process = start_datetime + process_after
-    duration = end_datetime - start_process
-    return {
-        "item_id": item_id,
-        "start_datetime": start_datetime,
-        "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
-        "process_after": process_after,
-        "start_process": start_process,
-        "duration": duration,
-    }
+    return {"ads_id": ads_id}
