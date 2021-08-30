1bf6a1ac4b4d03c36ff59362b0f3613b7d0f7b02
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-30 12:25:03 +0530

    Adden user input along with email in response body

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from typing import List, Optional
+from typing import Optional
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-    tags: List[str] = []
+
+class UserIn(BaseModel):
+
+    username: str
+
+    password: str
+
+    email: EmailStr
+    full_name: Optional[str] = None
 
 
-
-@app.post("/items/", response_model=Item)
-
-async def create_item(item: Item):
-    return item
-
+# Don't do this in production!
+@app.post("/user/", response_model=UserIn)
+async def create_user(user: UserIn):
+    return user
