2eddb576ff511feaaf89c9fe0961925c909a8b2e
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-30 19:01:07 +0530

    Added RequestValidationError

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, HTTPException
 
+from fastapi.exceptions import RequestValidationError
 
-
-class UnicornException(Exception):
-
-    def __init__(self, name: str):
-
-        self.name = name
-
-
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
 
-
-@app.exception_handler(UnicornException)
-
-async def unicorn_exception_handler(request: Request, exc: UnicornException):
-
-    return JSONResponse(
-
-        status_code=418,
-
-        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
-
-    )
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
 
 
-@app.get("/unicorns/{name}")
-async def read_unicorn(name: str):
-    if name == "yolo":
+@app.exception_handler(RequestValidationError)
 
-        raise UnicornException(name=name)
+async def validation_exception_handler(request, exc):
 
-    return {"unicorn_name": name}
+    return PlainTextResponse(str(exc), status_code=400)
+
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
