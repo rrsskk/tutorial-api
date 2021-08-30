5e444a1f5fe2a8294c51083f58ce06c5b08638bb
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-30 14:25:47 +0530

    Used list instead of Set() along with response_model_include and response_model_exclude

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -8,24 +8,37 @@ app = FastAPI()
 
 class Item(BaseModel):
     name: str
-
     description: Optional[str] = None
-
     price: float
-
     tax: float = 10.5
-
-    tags: List[str] = []
-
 
 
 items = {
     "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+    "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
+    "baz": {
+        "name": "Baz",
+        "description": "There goes my baz",
+        "price": 50.2,
+        "tax": 10.5,
+    },
 }
 
 
-@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
-async def read_item(item_id: str):
+@app.get(
+    "/items/{item_id}/name",
+    response_model=Item,
+
+    response_model_include=["name", "description"],
+
+)
+async def read_item_name(item_id: str):
     return items[item_id]
+
+
+
+@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude=["tax"])
+
+async def read_item_public_data(item_id: str):
+    return items[item_id]
+
