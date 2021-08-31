a3c57935f01f57cabfb513c046432732391b7bfb
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-31 11:48:55 +0530

    Used status_code in response body for path operation confrigation

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
-from fastapi import FastAPI, HTTPException
+from typing import Optional, Set
 
-from fastapi.exception_handlers import (
 
-    http_exception_handler,
+from fastapi import FastAPI, status
 
-    request_validation_exception_handler,
-
-)
-
-from fastapi.exceptions import RequestValidationError
-from starlette.exceptions import HTTPException as StarletteHTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.exception_handler(StarletteHTTPException)
-async def custom_http_exception_handler(request, exc):
-    print(f"OMG! An HTTP error!: {repr(exc)}")
-
-    return await http_exception_handler(request, exc)
-
-
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc):
-    print(f"OMG! The client sent invalid data!: {exc}")
-
-    return await request_validation_exception_handler(request, exc)
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+    tags: Set[str] = []
 
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    if item_id == 3:
-        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
-    return {"item_id": item_id}
+@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED)
+
+async def create_item(item: Item):
+    return item
 
