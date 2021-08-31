7a6541f0507a1020c4c0c24f5f92e8d2a40d5198
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-31 13:00:24 +0530

    Marked path operation as depricated

--- main.py
+++ main.py
@@ -1,42 +1,24 @@
-from typing import Optional, Set
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-    tags: Set[str] = []
+@app.get("/items/", tags=["items"])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
 
 
-@app.post("/items/", response_model=Item, summary="Create an item",
-           response_description="The created item")
-async def create_item(item: Item):
-
-    """
-
-    Create an item with all the information:
+@app.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "johndoe"}]
 
 
 
-    - **name**: each item must have a name
+@app.get("/elements/", tags=["items"], deprecated=True)
 
-    - **description**: a long description
+async def read_elements():
+    return [{"item_id": "Foo"}]
 
-    - **price**: required
-
-    - **tax**: if the item doesn't have tax, you can omit this
-
-    - **tags**: a set of unique tag strings for this item
-
-    """
-
-    return item
 
 
 
