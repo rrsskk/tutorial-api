90a5373ed0b62484ef833d83b4f3c78c62009c49
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-31 11:11:03 +0530

    Used RequestValidationError along with Body

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
-
-from fastapi.responses import PlainTextResponse
-
-from starlette.exceptions import HTTPException as StarletteHTTPException
-
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-
-@app.exception_handler(StarletteHTTPException)
-
-async def http_exception_handler(request, exc):
-
-    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
-
-
-
 @app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc):
-    return PlainTextResponse(str(exc), status_code=400)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+
+    )
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    if item_id == 3:
+class Item(BaseModel):
+    title: str
+    size: int
 
-        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
 
-    return {"item_id": item_id}
-
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
