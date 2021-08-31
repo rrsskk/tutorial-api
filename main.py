31092e8b60db13d40978f624ce4170022be5c460
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-31 16:59:29 +0530

    Created a sub-dependencies query_or_cookie_extractor

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 from typing import Optional
 
-from fastapi import Depends, FastAPI
+from fastapi import Cookie, Depends, FastAPI
 
 app = FastAPI()
 
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+def query_extractor(q: Optional[str] = None):
+    return q
 
 
+def query_or_cookie_extractor(
 
-class CommonQueryParams:
+    q: str = Depends(query_extractor), last_query: Optional[str] = Cookie(None)
 
-    def __init__(self, q: Optional[str] = None, skip: int = 0, limit: int = 100):
-
-        self.q = q
-
-        self.skip = skip
-
-        self.limit = limit
-
+):
+    if not q:
+        return last_query
+    return q
 
 
 @app.get("/items/")
-async def read_items(commons: CommonQueryParams = Depends()):
-    response = {}
-    if commons.q:
-        response.update({"q": commons.q})
-    items = fake_items_db[commons.skip : commons.skip + commons.limit]
-    response.update({"items": items})
-    return response
+async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
+    return {"q_or_cookie": query_or_default}
 
 
 
