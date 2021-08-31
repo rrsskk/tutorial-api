217e4e2925b3949fd435ce8211ae6ebbd97032e2
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-31 14:03:42 +0530

    Added PUT() for update repalcing in body

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
-from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 
 from fastapi import FastAPI
-
 from fastapi.encoders import jsonable_encoder
-
 from pydantic import BaseModel
-
-fake_db = {}
-
-
-class Item(BaseModel):
-    title: str
-    timestamp: datetime
-    description: Optional[str] = None
-
 
 app = FastAPI()
 
 
-@app.put("/items/{id}")
-def update_item(id: str, item: Item):
+class Item(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    tax: float = 10.5
+    tags: List[str] = []
 
-    json_compatible_item_data = jsonable_encoder(item)
 
-    fake_db[id] = json_compatible_item_data
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+
+@app.put("/items/{item_id}", response_model=Item)
+
+async def update_item(item_id: str, item: Item):
+
+    update_item_encoded = jsonable_encoder(item)
+
+    items[item_id] = update_item_encoded
+
+    return update_item_encoded
 
 
 
