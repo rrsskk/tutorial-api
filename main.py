aac8aee703e21620f2c9bda66b8530a3ba192936
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-08-31 13:53:37 +0530

    Used jsonable_encoder to convert datetime & pydentic model in str and dict() respectively

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
+from datetime import datetime
+from typing import Optional
+
 from fastapi import FastAPI
+
+from fastapi.encoders import jsonable_encoder
+
+from pydantic import BaseModel
+
+fake_db = {}
+
+
+class Item(BaseModel):
+    title: str
+    timestamp: datetime
+    description: Optional[str] = None
+
 
 app = FastAPI()
 
 
-@app.get("/items/", tags=["items"])
-async def read_items():
-    return [{"name": "Foo", "price": 42}]
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
 
+    json_compatible_item_data = jsonable_encoder(item)
 
-@app.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "johndoe"}]
-
-
-
-@app.get("/elements/", tags=["items"], deprecated=True)
-
-async def read_elements():
-    return [{"item_id": "Foo"}]
-
+    fake_db[id] = json_compatible_item_data
 
 
 
