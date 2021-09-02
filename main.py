756c27b4577325aae29cefde8b372e445ca655b0
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-09-02 13:15:47 +0530

    Used debugging vs code

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
-from typing import Optional
 
-from fastapi import FastAPI, Header, HTTPException
-from pydantic import BaseModel
+import uvicorn
 
-fake_secret_token = "coneofsilence"
-
-fake_db = {
-    "foo": {"id": "foo", "title": "Foo", "description": "There goes my hero"},
-    "bar": {"id": "bar", "title": "Bar", "description": "The bartenders"},
-}
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    id: str
-    title: str
-    description: Optional[str] = None
+@app.get("/")
+def root():
+    a = "a"
+    b = "b" + a
+    return {"hello world": b}
 
 
-@app.get("/items/{item_id}", response_model=Item)
-async def read_main(item_id: str, x_token: str = Header(...)):
-    if x_token != fake_secret_token:
-        raise HTTPException(status_code=400, detail="Invalid X-Token header")
-    if item_id not in fake_db:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return fake_db[item_id]
+if __name__ == "__main__":
 
-
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item, x_token: str = Header(...)):
-    if x_token != fake_secret_token:
-        raise HTTPException(status_code=400, detail="Invalid X-Token header")
-    if item.id in fake_db:
-        raise HTTPException(status_code=400, detail="Item already exists")
-    fake_db[item.id] = item
-    return item
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+ 
