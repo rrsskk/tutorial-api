b6753209a9b95405606f808eb48a6ce303b5c606
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-09-02 12:56:26 +0530

    Extended fastapi app file for testing using token-x

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
-from fastapi import FastAPI
+from typing import Optional
 
-from fastapi.testclient import TestClient
+from fastapi import FastAPI, Header, HTTPException
+from pydantic import BaseModel
 
+fake_secret_token = "coneofsilence"
+
+fake_db = {
+    "foo": {"id": "foo", "title": "Foo", "description": "There goes my hero"},
+    "bar": {"id": "bar", "title": "Bar", "description": "The bartenders"},
+}
 
 app = FastAPI()
 
 
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
+class Item(BaseModel):
+    id: str
+    title: str
+    description: Optional[str] = None
 
 
+@app.get("/items/{item_id}", response_model=Item)
+async def read_main(item_id: str, x_token: str = Header(...)):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return fake_db[item_id]
 
-client = TestClient(app)
 
-
-
-
-def test_read_main():
-
-    response = client.get("/")
-
-    assert response.status_code == 200
-
-    assert response.json() == {"msg": "Hello World"}
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item, x_token: str = Header(...)):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item.id in fake_db:
+        raise HTTPException(status_code=400, detail="Item already exists")
+    fake_db[item.id] = item
+    return item
