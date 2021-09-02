44949522cc7865d76b48b24145a39da0efcbf420
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-09-02 11:30:41 +0530

    Added metadata for API

--- main.py
+++ main.py
@@ -1,21 +1,66 @@
-from fastapi import BackgroundTasks, FastAPI
+from fastapi import FastAPI
 
 
-app = FastAPI()
+description = """
 
-
-def write_notification(email: str, message=""):
-    with open("log.txt", mode="w") as email_file:
-        content = f"notification for {email}: {message}"
-        email_file.write(content)
-
-
-@app.post("/send-notification/{email}")
-
-async def send_notification(email: str, background_tasks: BackgroundTasks):
-
-    background_tasks.add_task(write_notification, email, message="some notification")
-    return {"message": "Notification sent in the background"}
+ChimichangApp API helps you do awesome stuff. 🚀
 
 
 
+## Items
+
+
+
+You can **read items**.
+
+
+
+## Users
+
+
+
+You will be able to:
+
+
+
+* **Create users** (_not implemented_).
+
+* **Read users** (_not implemented_).
+
+"""
+
+
+app = FastAPI(
+
+    title="ChimichangApp",
+
+    description=description,
+
+    version="0.0.1",
+
+    terms_of_service="http://example.com/terms/",
+
+    contact={
+
+        "name": "Deadpoolio the Amazing",
+
+        "url": "http://x-force.example.com/contact/",
+
+        "email": "dp@x-force.example.com",
+
+    },
+
+    license_info={
+
+        "name": "Apache 2.0",
+
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+
+    },
+
+)
+
+
+@app.get("/items/")
+async def read_items():
+    return [{"name": "Katana"}]
