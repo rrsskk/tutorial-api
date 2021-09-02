8acfa2e5ab6770fc8b30d6dae23bde461d286a99
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-09-02 12:49:47 +0530

    Used Testclient for filetest

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
 
-from fastapi.staticfiles import StaticFiles
+from fastapi.testclient import TestClient
 
 
 app = FastAPI()
 
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+@app.get("/")
+async def read_main():
+    return {"msg": "Hello World"}
+
+
+
+client = TestClient(app)
+
+
+
+
+def test_read_main():
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    assert response.json() == {"msg": "Hello World"}
