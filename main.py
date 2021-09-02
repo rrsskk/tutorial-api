46c0aeb0cd18c90330f56ff7248bdf362460be5b
Ravi Shankar <ravi.kumar4@squareyards.co.in>
2021-09-02 11:40:08 +0530

    Added Open_API_URL

--- main.py
+++ main.py
@@ -1,45 +1,10 @@
 from fastapi import FastAPI
 
 
-tags_metadata = [
-
-    {
-
-        "name": "users",
-
-        "description": "Operations with users. The **login** logic is also here.",
-
-    },
-
-    {
-
-        "name": "items",
-
-        "description": "Manage items. So _fancy_ they have their own docs.",
-
-        "externalDocs": {
-
-            "description": "Items external docs",
-
-            "url": "https://fastapi.tiangolo.com/",
-
-        },
-
-    },
-
-]
+app = FastAPI(openapi_url="/api/v1/openapi.json")
 
 
 
-app = FastAPI(openapi_tags=tags_metadata)
-
-
-
-@app.get("/users/", tags=["users"])
-async def get_users():
-    return [{"name": "Harry"}, {"name": "Ron"}]
-
-
-@app.get("/items/", tags=["items"])
-async def get_items():
-    return [{"name": "wand"}, {"name": "flying broom"}]
+@app.get("/items/")
+async def read_items():
+    return [{"name": "Foo"}]
